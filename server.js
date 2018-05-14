5333ef4d877eeeb92b9ee9cd410c8603732ddb44
Susanna <Susanna.Huang@timeinc.com>
2018-05-14 15:43:57 -0400

    port for heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(volleyball);
 
 app.use(express.static(__dirname));
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Server listening on port', 3000);
 });
 
